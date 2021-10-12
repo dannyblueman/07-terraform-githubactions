35ff68b5644cb14cba4cbc3b992adaed412f1c94
EC2 Default User <ec2-user@ip-172-31-0-105.eu-north-1.compute.internal>
2021-10-12 10:22:55 +0000

    yaya

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> DevOpsForEver !
+          Edit <code>src/App.js</code> DevOpsForEverALways !
         </p>
         <a
           className="App-link"
