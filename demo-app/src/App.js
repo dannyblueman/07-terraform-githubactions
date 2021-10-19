784c42542aed988cf4b5258f6b8232f91916e5a1
dannyblueman <dhb_90@hotmail.com>
2021-10-19 09:41:27 +0000

    newtest

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> DevOpsForEverALways ! hilsen Daniel
+          Edit <code>src/App.js</code> DevOpsForEverALways ! Nå funker alt, hilsen Daniel
         </p>
         <a
           className="App-link"
